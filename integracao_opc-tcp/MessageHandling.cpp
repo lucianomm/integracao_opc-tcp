5a344ef200184640b04a576464475de808888b23
LUCIANO MOTA MORAES <lucianomm98@gmail.com>
2022-01-15 11:01:45 -0300

    Adding "to string" method to messageHandling

--- integracao_opc-tcp/MessageHandling.cpp
+++ integracao_opc-tcp/MessageHandling.cpp
@@ -1,8 +1,11 @@
 #include "MessageHandling.h"
-
 #include <string>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 #pragma once
+#define SEQUENCE_FIELD_SIZE 6
+#define REAL_FIELD_SIZE 6
 void MessageHandling::ConvertMessageHeader() {
 
 	messageHeader.SequenceNumber = stoi(rawMessage.substr(0, rawMessage.find('$')));
@@ -14,6 +17,7 @@ void MessageHandling::ConvertMessageHeader() {
 	return;
 }
 void MessageHandling::ConvertSetPointsMessage() {
+	isSetPointsMessage = true;
 	setPointsMessage.GasInjectionPressureSP = stod(rawMessage.substr(0, rawMessage.find('$')));
 	rawMessage.erase(0, rawMessage.find('$') + 1);
 
@@ -26,7 +30,7 @@ void MessageHandling::ConvertSetPointsMessage() {
 	return;
 }
 void MessageHandling::ConvertProcessDataMessage() {
-
+	isProcessDataMessage = true;
 	processDataMessage.LadleTemperature = stod(rawMessage.substr(0, rawMessage.find('$')));
 	rawMessage.erase(0, rawMessage.find('$') + 1);
 
@@ -43,4 +47,52 @@ void MessageHandling::ConvertProcessDataMessage() {
 }
 MessageHandling::MessageHandling(string RawMessage) : rawMessage(RawMessage) {
 	ConvertMessageHeader();
+}
+
+string MessageHandling::MessageHeaderToString() {
+	string sequenceNumber = to_string(messageHeader.SequenceNumber);
+	return string(SEQUENCE_FIELD_SIZE - sequenceNumber.length(), '0').append(sequenceNumber) + '$' +
+		to_string(messageHeader.MessageCode);
+
+}
+
+string MessageHandling::RealToString(double real) {
+	stringstream stream;
+	stream << fixed << setprecision(1) << real;
+	string result = stream.str();
+	return string(REAL_FIELD_SIZE - result.length(), '0').append(result);
+}
+
+string MessageHandling::ProcessDataMessageToString() {
+	string sequenceNumber = to_string(messageHeader.SequenceNumber);
+	string message =
+		MessageHeaderToString() + '$' +
+		RealToString(processDataMessage.LadleTemperature) + '$' +
+		RealToString(processDataMessage.VaccumChamberTemperature) + '$' +
+		RealToString(processDataMessage.GasInjectionPressure) + '$' +
+		RealToString(processDataMessage.VaccumChamberPressure);
+	return message;
+}
+
+string MessageHandling::SetPointsMessagetoString() {
+	string sequenceNumber = to_string(messageHeader.SequenceNumber);
+	string message =
+		MessageHeaderToString() + '$' +
+		RealToString(setPointsMessage.GasInjectionPressureSP) + '$' +
+		RealToString(setPointsMessage.VaccumChamberTemperatureSP) + '$' +
+		to_string(setPointsMessage.VaccumChamberPressureSP);
+	return message;
+}
+
+string MessageHandling::ACKMessageToString() {
+	string message =
+		to_string(messageHeader.SequenceNumber) + '$' +
+		to_string(messageHeader.MessageCode);
+	return message;
+}
+
+string MessageHandling::toString() {
+	if (isProcessDataMessage) return ProcessDataMessageToString();
+	else if (isSetPointsMessage) return SetPointsMessagetoString();
+	else return ACKMessageToString();
 }
